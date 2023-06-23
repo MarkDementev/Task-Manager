12a3e337215dfbc0d69c2edc35b5f1e95c0d9c8c
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-06-23 15:46:45 +0300

    remove tabs

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+	    SpringApplication.run(AppApplication.class, args);
+    }
 }
