842312d078cb134828c2eafcf364c5647fecfd0f
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-06-24 16:05:24 +0300

    add prod/dev configs

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package hexlet.code.app.controllers;
+package hexlet.code.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
