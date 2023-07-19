1268baaec95decf1928510d829d7b72d9745d00f
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-07-19 11:12:27 +0300

    delete unusual imports

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,7 +1,5 @@
 package hexlet.code.controller;
 
-import com.rollbar.notifier.Rollbar;
-import hexlet.code.config.rollbar.RollbarConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
